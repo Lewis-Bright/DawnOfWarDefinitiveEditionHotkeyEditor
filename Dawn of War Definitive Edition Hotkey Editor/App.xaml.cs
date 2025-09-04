5316d0ba7f4102f3d101a97b39aa39283111a361
Lewis Bright <lewis.john.bright95@gmail.com>
2025-09-04 15:42:47 +0100

    Init

---
+++ Dawn of War Definitive Edition Hotkey Editor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Dawn_of_War_Definitive_Edition_Hotkey_Editor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
